a477cc2a8264badf165703efb94299364537742d
Aidan Dyga <aidan.dyga@gmail.com>
2025-02-19 18:52:47 -0500

    fix: removed timing from c code

--- c/buffer-overflow.c
+++ c/buffer-overflow.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
 #include <string.h>
-#include <time.h>
 
 #define SIZE 10
 
 void buffer_overflow() {
     char buffer[SIZE];
-    struct timespec start, end;
-
-    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
 
     for (int i = 0; i <= SIZE; i++) {  // Out-of-bounds access!
         buffer[i] = 'A';  
     }
 
-    clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-    double elapsed = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
-    printf("Buffer Overflow executed in %.12lf seconds\n", elapsed);
+    printf("Buffer Overflow executed\n");
 }
 
 int main() {
--- c/null-pointer-dereference.c
+++ c/null-pointer-dereference.c
@@ -1,17 +1,9 @@
 #include <stdio.h>
-#include <time.h>
 
 void null_pointer_dereference() {
     int* ptr = NULL;
-    struct timespec start, end;
-
-    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
-
     int value = *ptr;  // Crash! Dereferencing NULL.
-
-    clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-    double elapsed = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
-    printf("Null Pointer Dereference executed in %.12lf seconds\n", elapsed);
+    printf("Null Pointer Dereference executed\n");
 }
 
 int main() {
