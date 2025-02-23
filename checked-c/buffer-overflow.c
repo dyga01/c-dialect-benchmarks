7fc49483fd1e9965e366e4efc61cdd791a09d27b
Aidan Dyga <aidan.dyga@gmail.com>
2025-02-22 21:30:47 -0500

    fix: updating the checked-c dockerfile to work correctly

--- checked-c/buffer-overflow.c
+++ checked-c/buffer-overflow.c
@@ -1,36 +1,6 @@
 #include <stdio_checked.h>
-#include <string_checked.h>
-#include <time_checked.h>
-#include <stdchecked.h>
-#include <stdlib_checked.h>
-
-#define SIZE 10
-
-#pragma CHECKED_SCOPE ON
-
-void buffer_overflow(void) { // Add void to the function prototype
-    nt_array_ptr<char> buffer : count(SIZE) = _Assume_bounds_cast<nt_array_ptr<char>>(malloc(SIZE * sizeof(char)));
-    if (!buffer) {
-        fputs("Memory allocation failed\n", stderr);
-        return;
-    }
-    
-    struct timespec start, end;
-    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
-    
-    // Safe bounded loop
-    for (int i = 0; i < SIZE; i++) {
-        buffer[i] = 'A';  
-    }
-    
-    clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-    double elapsed = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
-    printf("Buffer Overflow executed in %.12lf seconds\n", elapsed);
-    
-    free(buffer);
-}
 
 int main(void) {
-    buffer_overflow();
+    printf("Hello, World!\n");
     return 0;
 }
