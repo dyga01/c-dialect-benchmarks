6d8a57f98a54b79158b6e507eff457c02d2ca2fe
Aidan Dyga <aidan.dyga@gmail.com>
2025-02-26 21:08:11 -0500

    fix: updating rust buffer overflow

--- rust/buffer-overflow.rs
+++ rust/buffer-overflow.rs
@@ -1,14 +1,10 @@
-use std::ptr;
-
 const SIZE: usize = 10;
 
 fn buffer_overflow() {
-    let mut buffer = vec![0u8; SIZE];
-
+    let mut buffer = [0u8; SIZE];
     for i in 0..=SIZE {  // Out-of-bounds access!
-        buffer[i] = b'A';  
+        buffer[i] = b'A';
     }
-
     println!("Buffer Overflow executed");
 }
 
